228299f43e98a98ae467cb101f347fbe097c3ba8
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2024-09-23 17:14:42 -0300

    23-09

--- atividades/AT04_Lucas_Calixto_23-09/At01.py
+++ atividades/AT04_Lucas_Calixto_23-09/At01.py
@@ -3,7 +3,6 @@ def reverte():
     numero = int(input("Digite um número inteiro: "))
 
     numero = str(numero)
-    for i in range(len(numero)-1, -1, -1):
-        print(numero[i], end="")
+    print(f"O número {numero} invertido é {numero[::-1]}.")
 
 reverte()
