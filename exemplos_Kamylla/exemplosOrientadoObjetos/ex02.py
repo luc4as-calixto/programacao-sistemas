317bf4ea6eee69608ce4a0b423bc13e5f023e9d5
luc4as-calixto <lucas_calixto-souza@estudante.sesisenai.org.br>
2024-10-21 15:02:43 -0300

    21-10 lucas

---
+++ exemplos_Kamylla/exemplosOrientadoObjetos/ex02.py
@@ -0,0 +1,43 @@
+class ContaCorrente():
+
+    def __init__(self, nome, cpf, limite):
+        self.nome = nome
+        self.cpf = cpf
+        self.saldo = 0
+        self.limite = None
+
+    def consultar_saldo(self):
+        print('Seu saldo atual é de R${:,.2f}'.format(self.saldo))
+        pass
+
+    def depositar_dinheiro(self, valor):
+        self.saldo += valor
+        pass
+
+    def limite_conta(self):
+        self.limite = -1000
+        return self.limite
+
+    def sacar_dinheiro(self, valor):
+        if self.saldo - valor < self.limite_conta():
+            print('Você não tem saldo suficiente para sacar esse valor')
+            self.consultar_saldo()
+        else:
+            self.saldo -= valor
+        pass
+
+#Programa
+
+conta_lira = ContaCorrente("Lira", "111.222.333.45")
+conta_lira.consultar_saldo()
+
+#Depositar um dinheirinho na conta:
+conta_lira.depositar_dinheiro(10000)
+conta_lira.consultar_saldo()
+
+#Sacando um dinheirinho da conta:
+conta_lira.sacar_dinheiro(100000)
+conta_lira.consultar_saldo()
+
+print('Saldo da conta é',conta_lira.saldo)
+print(conta_lira.cpf)
